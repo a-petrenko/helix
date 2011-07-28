0862953738d6273767089c0accfb0035ce02d274
Andrei Petrenko <me@apetrenko.com>
2011-07-28 21:36:00 +0300

    Added Account model

---
+++ app/models/account.rb
@@ -0,0 +1,11 @@
+class Account
+  include Mongoid::Document
+
+  field :provider,  type: String
+  field :uid,       type: String
+  field :name,      type: String
+  field :email,     type: String
+
+  attr_protected :provider, :uid, :name, :email
+
+end
