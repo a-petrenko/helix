0a8f6bd2faca1caea86cd549e45a5140aebeb69a
Andrei Petrenko <me@apetrenko.com>
2011-07-28 21:03:14 +0300

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,12 @@
+source 'http://rubygems.org'
+
+gem 'rails', '~> 3.0.9'
+
+gem 'rspec-rails'
+gem 'cucumber-rails'
+gem 'capybara'
+gem 'mongoid'
+gem 'bson_ext'
+gem 'omniauth'
+
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Helix::Application.config.secret_token = 'a6df2b86bdcc8da16ca660de7c2fa3f4702b928701828e1e51522ceaa87572a93174de17051941e4f3fd985997c2287ae7ae15564e7b23690054be8e7556de98'
